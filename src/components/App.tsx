a4a42bcc0e22c38450780947de1498b16c8f3dfe
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-26 00:12:42 +0200

    Exports index file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './home/HomePage';
-import AboutPage from './about/AboutPage';
-import Header from './common/Header';
-import Footer from './common/Footer';
-import CoursesPage from './courses/CoursesPage';
+import { AboutPage, CoursesPage, Footer, Header, HomePage } from './';
 
 class App extends Component {
   render() {
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import AboutPage from './about/AboutPage';
+import Footer from './common/Footer';
+import Header from './common/Header';
+import CoursesPage from './courses/CoursesPage';
+import HomePage from './home/HomePage';
+
+export { AboutPage, Footer, Header, CoursesPage, HomePage };
